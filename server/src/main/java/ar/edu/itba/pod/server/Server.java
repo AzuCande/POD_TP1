f0b2e6b457eef5befe1ca69a62b4c5c95e9d9f80
Uriel <umihura@itba.edu.ar>
2022-09-03 16:33:07 -0300

    Feat: start csv processing

--- server/src/main/java/ar/edu/itba/pod/server/Server.java
+++ server/src/main/java/ar/edu/itba/pod/server/Server.java
@@ -1,12 +1,21 @@
 package ar.edu.itba.pod.server;
 
+import ar.edu.itba.pod.server.service.FlightManager;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.FileNotFoundException;
+
 
 public class Server {
     private static Logger logger = LoggerFactory.getLogger(Server.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         logger.info("rmi-project Server Starting ...");
+
+        FlightManager flightManager = new FlightManager();
+        flightManager.addPlaneModel();
+
     }
 }
--- server/src/main/java/ar/edu/itba/pod/server/service/FlightManager.java
+++ server/src/main/java/ar/edu/itba/pod/server/service/FlightManager.java
@@ -2,16 +2,32 @@ package ar.edu.itba.pod.server.service;
 
 import ar.edu.itba.pod.server.model.FlightState;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+
+import java.io.FileReader;
 
 public class FlightManager {
     Set<String> planeModels = ConcurrentHashMap.newKeySet(); // TODO chequear que garantiza exactamente
     Set<String> flightCodes = ConcurrentHashMap.newKeySet();
     Set<String> airports = ConcurrentHashMap.newKeySet();
 
-    public void addPlaneModel(){
+    public void addPlaneModel() throws FileNotFoundException {
         //consume CSV with plane models
+        String fileName = "D:\\itba\\pod\\POD_TP1\\server\\src\\main\\resources\\csv\\planeModels.csv"; //TODO: hacer relative path
+        try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
+            List<String[]> r = reader.readAll();
+            r.forEach(x -> System.out.println(Arrays.toString(x)));
+            r.forEach(x -> planeModels.add(Arrays.toString(x)));
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+        }
     }
 
     public void addFlight(){
