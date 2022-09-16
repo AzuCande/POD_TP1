ec9af06500791b36fce2be5828c22eb5192af5e9
patrickmdey <patrickmdey@hotmail.com>
2022-09-15 21:41:42 -0300

    Refactor: bash files

--- compile.sh
+++ compile.sh
@@ -8,10 +8,10 @@ mvn clean install
 cd server/target
 tar -xzf tpe1-g6-server-1.0-SNAPSHOT-bin.tar.gz
 cd tpe1-g6-server-1.0-SNAPSHOT
-chmod +x *.sh
+chmod +x run-registry run-server
 
 cd $TP_PATH/client/target
 tar -xzf tpe1-g6-client-1.0-SNAPSHOT-bin.tar.gz
 cd tpe1-g6-client-1.0-SNAPSHOT
-chmod +x *.sh
+chmod +x run-admin run-notifications run-seatAssign run-seatMap
 cd $TP_PATH
